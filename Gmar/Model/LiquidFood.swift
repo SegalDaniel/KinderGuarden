cd2f6927da8b6a0e0d18bd3b30a36ab837e9704f
segaldaniel <dannysegal1@gmail.com>
2019-02-16 18:06:54 +0200

    Added food models

---
+++ Gmar/Model/LiquidFood.swift
@@ -0,0 +1,47 @@
+//
+//  LiquidFood.swift
+//  Gmar
+//
+//  Created by Daniel Segal on 16/02/2019.
+//  Copyright © 2019 Final Project. All rights reserved.
+//
+
+
+import UIKit
+import Foundation
+class LiquidFood{
+    var id:String
+    let mealType : String
+    let amount:String
+    let consumedAmount:String
+
+    
+    init(_id:String, _mealType:String, _amount:String, _consumedAmount:String){
+        self.id=_id
+        self.mealType=_mealType
+        self.amount=_amount
+        self.consumedAmount=_consumedAmount
+
+    }
+    
+    init(json:[String:Any]) {
+        id = json["id"] as! String
+        mealType = json["mealType"] as! String
+        amount = json["amount"] as! String
+        consumedAmount = json["consumedAmount"] as! String
+        
+    }
+    
+    
+    func toJson() -> [String:Any] {
+        var json = [String:Any]()
+        json["id"] = id
+        json["mealType"] = mealType
+        json["amount"] = amount
+        json["consumedAmount"] = consumedAmount
+        return json
+        
+    }
+    
+}
+
---
+++ Gmar/Model/Water.swift
@@ -0,0 +1,9 @@
+//
+//  Water.swift
+//  Gmar
+//
+//  Created by Daniel Segal on 16/02/2019.
+//  Copyright © 2019 Final Project. All rights reserved.
+//
+
+import Foundation
