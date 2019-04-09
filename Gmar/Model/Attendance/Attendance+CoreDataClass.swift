5781eb2d1feda0f871e53aac2923d7a4c140f974
segaldaniel <dannysegal1@gmail.com>
2019-04-09 14:24:45 +0300

    fixed bool type and added staff tp attemdance

--- Gmar/Model/Attendance/Attendance+CoreDataClass.swift
+++ Gmar/Model/Attendance/Attendance+CoreDataClass.swift
@@ -2,7 +2,7 @@
 //  Attendance+CoreDataClass.swift
 //  Gmar
 //
-//  Created by Daniel Segal on 28/03/2019.
+//  Created by Daniel Segal on 09/04/2019.
 //  Copyright © 2019 Final Project. All rights reserved.
 //
 //
@@ -12,13 +12,14 @@ import CoreData
 
 @objc(Attendance)
 public class Attendance: NSManagedObject {
-    convenience init(isLate:Bool, type:String, eventDate:String, child:Child?, staff:Staff?){
+    convenience init(isLate:Bool, type:String, eventDate:String, child:Child?, staff:Staff?, authorized:AuthorizedAccompanist?){
         self.init(entity: Model.instance.authorizedAccompanistEntity, insertInto: Model.instance.managedContext)
         self.setValue(isLate, forKey: "isLate")
         self.setValue(type, forKey: "type")
         self.setValue(eventDate, forKey: "eventDate")
         self.setValue(child, forKey: "child")
         self.setValue(child, forKey: "staff")
+        self.setValue(authorized, forKey: "authorized")
     }
     
     convenience  init(json:[String:Any]) {
@@ -26,6 +27,7 @@ public class Attendance: NSManagedObject {
         self.setValue(json["isLate"], forKey: "isLate")
         self.setValue(json["type"], forKey: "type")
         self.setValue(json["eventDate"], forKey: "eventDate")
+        self.setValue(json["authorized"], forKey: "authorized")
         
     }
     
@@ -36,6 +38,8 @@ public class Attendance: NSManagedObject {
         json["type"] = type
         json["eventDate"] = eventDate
         json["childID"] = child?.childID
+        json["staffID"] = staff?.staffID
+        json["authorized"] = authorized
         return json
         
     }
--- Gmar/Model/Attendance/Attendance+CoreDataProperties.swift
+++ Gmar/Model/Attendance/Attendance+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Attendance+CoreDataProperties.swift
 //  Gmar
 //
-//  Created by Daniel Segal on 28/03/2019.
+//  Created by Daniel Segal on 09/04/2019.
 //  Copyright © 2019 Final Project. All rights reserved.
 //
 //
@@ -17,9 +17,9 @@ extension Attendance {
         return NSFetchRequest<Attendance>(entityName: "Attendance")
     }
 
+    @NSManaged public var eventDate: NSDate?
     @NSManaged public var isLate: Bool
     @NSManaged public var type: String?
-    @NSManaged public var eventDate: NSDate?
     @NSManaged public var authorized: AuthorizedAccompanist?
     @NSManaged public var child: Child?
     @NSManaged public var staff: Staff?
