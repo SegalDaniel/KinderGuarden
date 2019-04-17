a29f6ad938f49b651dc7a70608833422a6db4020
Zach Bachar <zachbachar@gmail.com>
2019-04-17 18:05:27 +0300

    small fixes

--- Gmar/ViewControllers/Add Event Chain VCs/GenericLiquid/GenericLiquid_Confirmation.swift
+++ Gmar/ViewControllers/Add Event Chain VCs/GenericLiquid/GenericLiquid_Confirmation.swift
@@ -19,46 +19,52 @@ extension GenericLiquidViewController{
             showUnselectedAlert(dismiss: loadingAlert)
             return
         }
+        var val = "\(Int(quantitySlider.value) * 10)"
         let eventDate:Date = getEventDate()
         switch kind {
+        //MARK: - Milk
         case .milk:
             if let child = child{
                 if let staff = staff{
                     let eventType:Int16 = Int16(Enums.BasicEvent.milk.rawValue)
-                    let liquidFood = LiquidFood(mealType: "milk", amount: nil, consumedAmount: amount, eventType: eventType, eventDate: eventDate as NSDate, child: child, staff: staff)
+                    let liquidFood = LiquidFood(mealType: "milk", amount: nil, consumedAmount: val, eventType: eventType, eventDate: eventDate as NSDate, child: child, staff: staff)
                     Model.instance.sendToFB(basicEvent: liquidFood) { (err) in
                         self.sendToFBCallback(err, loadingAlert: loadingAlert)
                     }
                 }
             }
             break
+        //MARK: - Tamal
         case .tamal:
             if let child = child{
                 if let staff = staff{
                     let eventType:Int16 = Int16(Enums.BasicEvent.tamal.rawValue)
-                    let liquidFood = LiquidFood(mealType: "tamal", amount: nil, consumedAmount: amount, eventType: eventType, eventDate: eventDate as NSDate, child: child, staff: staff)
+                    let liquidFood = LiquidFood(mealType: "tamal", amount: nil, consumedAmount: val, eventType: eventType, eventDate: eventDate as NSDate, child: child, staff: staff)
                     Model.instance.sendToFB(basicEvent: liquidFood) { (err) in
                         self.sendToFBCallback(err, loadingAlert: loadingAlert)
                     }
                 }
             }
             break
+        //MARK: - Feever
         case .feever:
             if let child = child{
                 if let staff = staff{
+                    val = "\(Double(quantitySlider.value).rounded(toPlaces: 1))"
                     let eventType:Int16 = Int16(Enums.BasicEvent.feever.rawValue)
-                    let fever = Fever(tempreture: amount, eventType: eventType, eventDate: eventDate as NSDate, child: child, staff: staff)
+                    let fever = Fever(tempreture: val, eventType: eventType, eventDate: eventDate as NSDate, child: child, staff: staff)
                     Model.instance.sendToFB(basicEvent: fever) { (err) in
                         self.sendToFBCallback(err, loadingAlert: loadingAlert)
                     }
                 }
             }
             break
+        //MARK: - Water
         case .water:
             if let child = child{
                 if let staff = staff{
                     let eventType:Int16 = Int16(Enums.BasicEvent.water.rawValue)
-                    let water = Water(amount: nil, consumedAmount: amount, eventType: eventType, eventDate: eventDate as NSDate, child: child, staff: staff)
+                    let water = Water(amount: nil, consumedAmount: val, eventType: eventType, eventDate: eventDate as NSDate, child: child, staff: staff)
                     Model.instance.sendToFB(basicEvent: water) { (err) in
                         self.sendToFBCallback(err, loadingAlert: loadingAlert)
                     }
